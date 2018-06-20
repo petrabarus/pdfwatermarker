c7fa666cb23bd6b65a36758502a5c8d7d8b6ec11
Petra Barus <petra.barus@gmail.com>
2018-06-20 21:46:33 +0700

    Add option parser.

--- src/main/kotlin/net/petrabarus/pdfwatermarker/App.kt
+++ src/main/kotlin/net/petrabarus/pdfwatermarker/App.kt
@@ -1,5 +1,48 @@
 package net.petrabarus.pdfwatermarker
 
+import com.beust.jcommander.JCommander
+import com.beust.jcommander.Parameter
+import java.io.File
+
 fun main(args: Array<String>) {
-    println("Hello, World")
+
+    val app = App(args)
+
+    if (app.help) {
+        app.printUsage()
+        return
+    }
+}
+
+class App(args: Array<String>) {
+
+    @Parameter(names = arrayOf("-o", "--output"), required = false, description = "Output of the result, if empty will overwrite the PDF file")
+    lateinit var outputFilePath: String
+
+    @Parameter(names = arrayOf("--help"), help = true)
+    var help = false
+
+    //TODO: Validator
+    @Parameter(description = "<Path to PDF file> <Path to PNG stamp file>")
+    lateinit var params: String
+
+    private var jCommander: JCommander = JCommander.newBuilder()
+            .addObject(this)
+            .build()
+    init {
+        jCommander.parse(args.joinToString(" "))
+    }
+
+    val pdfFile: File
+        get() = File(params.split(" ")[0])
+
+    val stampFile: File
+        get() = File(params.split(" ")[1])
+
+    val outputFile: File
+        get() = if (outputFilePath != null) File(outputFilePath) else pdfFile
+
+    fun printUsage() {
+        jCommander.usage()
+    }
 }
--- src/test/kotlin/net/petrabarus/pdfwatermarker/AppTest.kt
+++ src/test/kotlin/net/petrabarus/pdfwatermarker/AppTest.kt
@@ -2,7 +2,23 @@ package net.petrabarus.pdfwatermarker
 
 import org.junit.Test
 import kotlin.test.assertEquals
+import kotlin.test.assertTrue
 
 class AppTest {
 
+    @Test
+    fun shouldShowHelp() {
+        val app = App(arrayOf("--help"))
+        assertEquals(true, app.help)
+    }
+
+    @Test
+    fun shouldHandleFileInputParams() {
+        val app = App(arrayOf(
+                "./src/test/resources/PDFSample.pdf",
+                "./src/test/resources/shape.png"
+        ))
+        assertTrue(app.pdfFile.isFile)
+        assertTrue(app.stampFile.isFile)
+    }
 }
